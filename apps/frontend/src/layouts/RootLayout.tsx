c5761c684eb4d38f5083c6fba91555604494cccd
shiyiwan <1057862180@qq.com>
2026-09-04 00:36:45 +0800

    develop left bar

--- apps/frontend/src/layouts/RootLayout.tsx
+++ apps/frontend/src/layouts/RootLayout.tsx
@@ -1,18 +1,34 @@
 import { Outlet } from "react-router";
-import { useResizablePanel } from "./useResizablePanel";
+import { useResizableTogglePanel } from "./useResizableTogglePanel";
+import { CircleFadingArrowUpIcon } from "lucide-react";
+import { Toggle } from "@/components/ui/toggle";
 
 
 
 export default function RootLayout() {
-    const { width, onPointerDown, onPointerMove, onPointerUp } = useResizablePanel(30)
+    const { width, onPointerDown, onPointerMove, onPointerUp, activePanel: activeLeftPanel, setActivePanel: setActiveLeftPanel } = useResizableTogglePanel(200, 100, 500)
+    const leftSidebarWidth = activeLeftPanel ? width : 0;
     return (
-        <div className="grid grid-cols-[auto_minmax(0,1fr)_300px] grid-rows-[auto_1fr] h-screen overflow-hidden">
-            <div className="col-span-3 h-12 bg-background">Header</div>
-            <aside className="relative border-r overflow-y-auto bg-sidebar"
-                style={{ width: width }}>
-                <div>左侧边栏</div>
-                <div className="absolute right-0 top-0 w-2  h-full
-                hover:bg-amber-100 delay-100 hover:cursor-col-resize"
+        <div className="grid grid-cols-[auto_auto_minmax(0,1fr)_300px] grid-rows-[auto_1fr_auto] h-screen overflow-hidden">
+            <div className="col-span-4 h-12 bg-background">
+                <Toggle><CircleFadingArrowUpIcon /></Toggle>
+                Header</div>
+            <div className="h-full overflow-hidden flex flex-col p-1 gap-1">  {/*only add scroll bar at inner container*/}
+                <Toggle pressed={activeLeftPanel === "explorer"} onPressedChange={(pressed) => setActiveLeftPanel(pressed ? "explorer" : null)}>
+                    <CircleFadingArrowUpIcon />
+                </Toggle>
+                <Toggle pressed={activeLeftPanel === "search"} onPressedChange={(pressed) => setActiveLeftPanel(pressed ? "search" : null)}>
+                    <CircleFadingArrowUpIcon />
+                </Toggle>
+                <Toggle pressed={activeLeftPanel === "git"} onPressedChange={(pressed) => setActiveLeftPanel(pressed ? "git" : null)}>
+                    <CircleFadingArrowUpIcon />
+                </Toggle>
+            </div>
+            <aside className="relative border-r overflow-hidden bg-sidebar"
+                style={{ width: leftSidebarWidth }}>
+                <div >左侧边栏</div>
+                <div className="absolute right-0 top-0 w-2 h-full
+                hover:bg-amber-100 delay-100 hover:cursor-ew-resize"
                     onPointerMove={onPointerMove}
                     onPointerDown={onPointerDown}
                     onPointerUp={onPointerUp}
@@ -25,6 +41,7 @@ export default function RootLayout() {
             <aside className="border-l overflow-y-auto bg-sidebar">
                 <div>右侧边栏</div>
             </aside>
+            <div className="col-span-4 h-12 bg-background">Footer</div>
         </div>
     )
 }
--- apps/frontend/src/layouts/useResizableTogglePanel.tsx
+++ apps/frontend/src/layouts/useResizableTogglePanel.tsx
@@ -1,19 +1,28 @@
 import { useRef, useState } from "react"
 
-export function useResizablePanel(defaultWidth: number) {
+export function useResizableTogglePanel(defaultWidth: number, minWidth: number, maxWidth: number) {
     const [width, setWidth] = useState(defaultWidth)
     const startX = useRef(0)
+    const startWidth = useRef(0)
+    const [activePanel, setActivePanel] = useState<string | null>(null);
+
     const onPointerDown = (e: React.PointerEvent<HTMLDivElement>) => {
         e.currentTarget.setPointerCapture(e.pointerId) //capture point, if not, event will lost when mouse leave handler div
         startX.current = e.clientX
+        startWidth.current = width
         document.body.style.userSelect = 'none' //prevent select text action
     }
 
     const onPointerMove = (e: React.PointerEvent<HTMLDivElement>) => {
         if (e.buttons !== 1) return //0=null;1=left;2=right;4=bottom;3=left+right
-        const delta = e.clientX - startX.current
-        setWidth(e.clientX)
-
+        const next = startWidth.current + e.clientX - startX.current
+        if (next > maxWidth) return
+        if (next < minWidth) {
+            setActivePanel(null)
+            setWidth(defaultWidth)
+            return
+        }
+        setWidth(Math.min(next, maxWidth))
     }
 
     const onPointerUp = (e: React.PointerEvent<HTMLDivElement>) => {
@@ -26,6 +35,8 @@ export function useResizablePanel(defaultWidth: number) {
         width,
         onPointerDown,
         onPointerMove,
-        onPointerUp
+        onPointerUp,
+        activePanel,
+        setActivePanel
     }
 }
