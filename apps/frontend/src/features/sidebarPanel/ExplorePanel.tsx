b939bf7ab1ade597e41bf09fd2aee43dbcb026cd
shiyiwan <1057862180@qq.com>
2026-09-06 23:53:11 +0800

    fileExplore

--- apps/frontend/src/features/sidebarPanel/ExplorePanel.tsx
+++ apps/frontend/src/features/sidebarPanel/ExplorePanel.tsx
@@ -1,4 +1,49 @@
-export default function ExplorePanel(){
+class FileItem {
+    public type: "Folder" | "File" | "Other";
+    public name: string;
+    public remark?: string;
+    public children: FileItem[] = [];
+
+    constructor(type: "Folder" | "File" | "Other", name: string);
+
+    constructor(type: "Folder" | "File" | "Other", name: string, remark?: string, children?: FileItem[]) {
+        if (type === "File" && children?.length != 0) {
+            throw Error("file can not have children")
+        }
+        this.type = type
+        this.name = name
+        remark ?? (this.remark = remark)
+        children ?? (this.remark = remark)
+    }
+
+}
+
+export default function ExplorePanel() {
+    const testData: FileItem[] = [
+        {
+            type: "Folder",
+            name: "folderA",
+            children: []
+        },
+        {
+            type: "Folder",
+            name: "folderB",
+            children: [{
+                type: "Folder",
+                name: "folderBA",
+                children: []
+            }, {
+                type: "File",
+                name: "File1",
+                children: []
+            }]
+        },
+
+    ]
+
+
+
+
     return (
         <div>ExplorePanel</div>
     )
