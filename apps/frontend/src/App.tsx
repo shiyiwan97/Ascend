aea0e75e0f266aec8e4a71d0e33713d75ee9ab41
shiyiwan <1057862180@qq.com>
2026-09-13 23:50:43 +0800

    water3

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,20 +2,13 @@ import './App.css'
 import { Routes, Route } from 'react-router'
 import NotePage from './pages/NotePage'
 import RootLayout from './layouts/RootLayout'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 
 function App() {
   return (
     <Routes>
-      <Route element={<RootLayout/>}>
-        <Route path="home" element={<h1>Home</h1>}/>
-        <Route path="note-page" element={<NotePage/>}/>
+      <Route element={<RootLayout />}>
+        <Route path="home" element={<h1>Home</h1>} />
+        <Route path="note-page" element={<NotePage />} />
       </Route>
     </Routes>
   )
@@ -25,4 +18,4 @@ export default App
 
 
 
- 
+
